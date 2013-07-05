72be76f70ffadb34bfbcd0ca3ac7beff04bf2a27
Karsten Becker <KB@Karsten-Becker.de>
2013-07-05 10:55:12 +0200

    Added GPL Banner

--- src/org/pshdl/interpreter/vcd/VCDCompareEngine.java
+++ src/org/pshdl/interpreter/vcd/VCDCompareEngine.java
@@ -1,3 +1,29 @@
+/*******************************************************************************
+ * PSHDL is a library and (trans-)compiler for PSHDL input. It generates
+ *     output suitable for implementation or simulation of it.
+ *     
+ *     Copyright (C) 2013 Karsten Becker (feedback (at) pshdl (dot) org)
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *     This License does not grant permission to use the trade names, trademarks,
+ *     service marks, or product names of the Licensor, except as required for 
+ *     reasonable and customary use in describing the origin of the Work.
+ * 
+ * Contributors:
+ *     Karsten Becker - initial API and implementation
+ ******************************************************************************/
 package org.pshdl.interpreter.vcd;
 
 import java.math.*;
@@ -16,13 +42,11 @@ public class VCDCompareEngine {
 	private final Set<InternalInformation> matched = new HashSet<InternalInformation>();
 	private final Set<InternalInformation> unmatched = new HashSet<InternalInformation>();
 	private final Map<String, VcdVariable> toVar = new HashMap<String, VcdVariable>();
-	private final ExecutableModel em;
 	private final VcdTimePeriod tp1;
 
 	public VCDCompareEngine(String file, ExecutableModel em, String psPrefix, String vcdPrefix) {
 		this.psPrefix = psPrefix;
 		this.vcdPrefix = vcdPrefix;
-		this.em = em;
 		vcdFile = new VcdFileParser(file);
 		// Reuse these objects in the hope of more performance.
 		tp1 = new VcdTimePeriod();
