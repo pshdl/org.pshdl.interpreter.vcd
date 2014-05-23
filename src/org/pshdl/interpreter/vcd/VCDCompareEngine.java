08f8d3e2b11cbee4fade0bcf7aa9b2fa1146ebea
Karsten Becker <KB@Karsten-Becker.de>
2014-05-23 13:31:44 +0200

    formatted import

--- src/org/pshdl/interpreter/vcd/VCDCompareEngine.java
+++ src/org/pshdl/interpreter/vcd/VCDCompareEngine.java
@@ -1,38 +1,45 @@
 /*******************************************************************************
  * PSHDL is a library and (trans-)compiler for PSHDL input. It generates
  *     output suitable for implementation or simulation of it.
- *     
+ *
  *     Copyright (C) 2013 Karsten Becker (feedback (at) pshdl (dot) org)
- * 
+ *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
- * 
+ *
  *     This program is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
- * 
+ *
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  *     This License does not grant permission to use the trade names, trademarks,
- *     service marks, or product names of the Licensor, except as required for 
+ *     service marks, or product names of the Licensor, except as required for
  *     reasonable and customary use in describing the origin of the Work.
- * 
+ *
  * Contributors:
  *     Karsten Becker - initial API and implementation
  ******************************************************************************/
 package org.pshdl.interpreter.vcd;
 
-import java.math.*;
-import java.util.*;
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
-import org.pshdl.interpreter.*;
+import org.pshdl.interpreter.ExecutableModel;
+import org.pshdl.interpreter.IHDLInterpreter;
+import org.pshdl.interpreter.InternalInformation;
 
-import vcdEngine.*;
+import vcdEngine.VcdFileParser;
+import vcdEngine.VcdTimePeriod;
 import vcdEngine.VcdTimescale.UNIT_ENUM;
+import vcdEngine.VcdVariableManager;
 import vcdEngine.VcdVariableManager.VcdVariable;
 
 public class VCDCompareEngine {
